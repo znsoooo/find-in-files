a1147af3a95461c052efb4f815930455643fc1fa
Shixian Li <lsx7@sina.com>
2024-05-19 10:16:53 +0800

    Chore: Adjust dist program

--- dist.bat
+++ dist.bat
@@ -2,18 +2,8 @@
 
 set name=FindInFiles
 set build=py -m PyInstaller -yws --noupx
-set quiet=1^>nul 2^>nul
 
 %build% %name%.py
-echo.
-
-echo Remove unused files...
-del dist\%name%\*.manifest %quiet%
-del dist\%name%\*.pyd %quiet%
-del dist\%name%\api*.dll %quiet%
-del dist\%name%\_internal\*.pyd %quiet%
-del dist\%name%\_internal\api*.dll %quiet%
-move dist\%name%\wx*.dll dist\%name%\wx %quiet%
 echo.
 
 dist\%name%\%name%.exe
